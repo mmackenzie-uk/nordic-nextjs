4e8860fd4dca71963c4088538e6f7f42a225479b
mark <mmackenzie.uk@gmail.com>
2025-04-16 10:50:15 +0100

    refactor

--- app/data/seed.js
+++ app/data/seed.js
@@ -1,62 +1,71 @@
-import categoriesSeedDTO from "./categories.json" with {type: "json"};
-import productsSeedDTO from "./products.json" with {type: "json"};
+import categories from "./categories.json" with {type: "json"};
+import seedDTOs from "./seedDTOs.json" with {type: "json"};
 
 import { openDb } from './db.js' 
 
-async function setup() {
- 
-    const db = await openDb()
-    // Define table schema
-
-    await db.exec(`
-      CREATE TABLE IF NOT EXISTS categories (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name VARCHAR(255) NOT NULL,
-      slug VARCHAR(255) NOT NULL
-      );
+async function createCategories() {
+  const db = await openDb()
+  // Define table schema
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    slug VARCHAR(255) NOT NULL
+    );
   `);
-
-   await db.exec(`
-      CREATE TABLE IF NOT EXISTS products (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name VARCHAR(255) NOT NULL,
-      smallImage VARCHAR(512),
-      mediumImage VARCHAR(512),
-      largeImage VARCHAR(512),
-      slug VARCHAR(255),
-      description VARCHAR(512),
-      price INTEGER NOT NULL,
-      availability INTEGER NOT NULL,
-      categoryId  INTEGER,
-      FOREIGN KEY (categoryId) REFERENCES categories(categoryId)
-       );
-   `);
-
   const arrCat = [];
-
-  categoriesSeedDTO.map(({name, slug}) => { 
+  categories.map(({name, slug}) => { 
     const pr = db.run(`INSERT INTO categories (name, slug) VALUES (?, ?)`, name, slug );
     arrCat.push(pr);
   })
   await Promise.all(arrCat);
+  await db.close();
+}
 
+async function toDomain() {
+  const db = await openDb()
   const categories = await db.all('SELECT * FROM categories');
-
-  const prodArr = [];
-
-  productsSeedDTO.map((productSeedDTO) => {
-    const { 
-      name, 
-      price, 
-      description, 
-      smallImage, 
-      mediumImage, 
-      largeImage, 
-      availability, 
-      slug
-    } = productSeedDTO;
+  const products = [];
+  seedDTOs.map((dataObject) => {
     // For ease of entering data, catgeory is entered by slug. Convert slug to Id
-    const category = categories.find(({ slug }) => (slug === productSeedDTO.category));
+    const category = categories.find(({ slug }) => (slug === dataObject.category));
+
+    const product = {
+      name: dataObject.name,
+      price: Number(dataObject.price * 100), 
+      description: dataObject.description, 
+      smallImage: JSON.stringify(dataObject.smallImage), 
+      mediumImage: JSON.stringify(dataObject.mediumImage), 
+      largeImage: JSON.stringify(dataObject.largeImage), 
+      availability: dataObject.availability, 
+      slug: dataObject.slug,
+      categoryId: category.id
+    }
+    products.push(product);
+  });
+  await db.close();
+  return products;
+}
+
+async function createProducts(products) {
+  const db = await openDb()
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS products (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    smallImage VARCHAR(512),
+    mediumImage VARCHAR(512),
+    largeImage VARCHAR(512),
+    slug VARCHAR(255),
+    description VARCHAR(512),
+    price INTEGER NOT NULL,
+    availability INTEGER NOT NULL,
+    categoryId  INTEGER,
+    FOREIGN KEY (categoryId) REFERENCES categories(categoryId)
+    );
+  `);
+  const prodArr = [];
+  products.map((product) => {
     const pr = db.run(
       `INSERT INTO products ( name, 
                               smallImage,
@@ -67,23 +76,29 @@ async function setup() {
                               availability,
                               price,
                               categoryId) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`, 
-                                name, 
-                                JSON.stringify(smallImage), 
-                                JSON.stringify(mediumImage), 
-                                JSON.stringify(largeImage), 
-                                slug,
-                                description,
-                                availability, 
-                                Number(price * 100), 
-                                category.id
+                              product.name, 
+                              product.smallImage, 
+                              product.mediumImage, 
+                              product.largeImage, 
+                              product.slug,
+                              product.description,
+                              product.availability, 
+                              product.price,
+                              product.categoryId
     );
     prodArr.push(pr);
   })
   await Promise.all(prodArr);
   await db.close();
+}
+
+async function seed() {
+  await createCategories();
+  const products = await toDomain();
+  await createProducts(products); 
 }  
 
-setup()
+seed()
   .catch(err => {
     console.error(err.message)
   })  
