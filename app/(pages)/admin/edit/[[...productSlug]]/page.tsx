5e62de93084900171ec18e72b78a5bf6d513758b
mark <mmackenzie.uk@gmail.com>
2025-04-20 19:27:46 +0100

    wip

--- app/(pages)/admin/edit/[[...productSlug]]/page.tsx
+++ app/(pages)/admin/edit/[[...productSlug]]/page.tsx
@@ -1,7 +1,6 @@
 
 import { getFormData } from "@/app/actions/form-actions";
 import { getCategories } from "@/app/actions/get-actions";
-import { getPhotos } from "@/app/actions/s3-bucket";
 import Form from "@/app/ui-client/form";
 import { _Object } from "@aws-sdk/client-s3";
 
@@ -13,7 +12,6 @@ export default async function Page({ params, }: {params: Promise<{ productSlug: 
         edit = true;
     }
     const categoriesDTO = await getCategories();
-    // const photos = await getPhotos() as _Object[];
 
     return <Form formDTO={formDTO} categoriesDTO={categoriesDTO} edit={edit} />
 
--- app/(pages)/test2/page.tsx
+++ app/(pages)/test2/page.tsx
@@ -1,22 +1,10 @@
-
 import { getAlbums } from "@/app/actions/s3-get-albums";
 import Photos from "./photos";
 
-// @ts-nocheck 
-
-
 export default async function Home() {
-
     const albums = await getAlbums();
-
-
     return (
         <section className="section">
-            <div className="edit-product-image-header" style={{maxWidth: "60%", marginBottom: "10px"}}>
-                <div>
-                    <h2 className="edit-product-image-header-title">Images:</h2>
-                </div>
-            </div>
             <Photos albums={albums!}/>
         </section>
     );
